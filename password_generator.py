827e6e467d77328416c283b0d5dcbabd87129ee5
Kuba <jakub@buchocki.pl>
2019-03-16 16:14:05 +0100

    Many changes to program structure.
    - Change password_gen_init func logic,
    - Add check_gen_coditions func (for checking conditions to generate,
    password)
    - Change generate_password func, add raise ValueError case,
    - Add copping password to clipboard (add copy function from pyperclip
    module)

--- password_generator.py
+++ password_generator.py
@@ -3,32 +3,67 @@
 
 import string
 from secrets import choice
+from pyperclip import copy
 
 
 def password_gen_init():
-    """
+    """Initialize generating password process.
+
     Ask user about password parameters (length and strength) and
     initializes process of generating password.
 
-    Prints generated password.
+    :return: string -> message containing generated password
     """
     try:
         length = int(input('\nEnter a passwords length: '))
         strength = int(input('\nEnter a passwords strength: '))
-        password = generate_password(length, strength)
 
-        if len(password) == int(length):
-            print('\n-----------------------------')
-            print('\nGenerated password is:\n')
-            print(password.center(30))
+        if check_gen_conditions(length, strength):
+            password = generate_password(length, strength)
+        else:
+            print('\nYou gave wrong parameters for password generating.'
+                  '\nPlease, check the NOTEs and try again.')
+            return password_gen_init()
+
+        if len(password) == length:
+            copy(password)  # Copping password to clipboard
+
+            password_message = '\n-----------------------------' \
+                               '\nGenerated password is:\n' \
+                               + password.center(30) \
+                               + '\n\n*Your password has been also copied to clipboard.'
+
+            return password_message
 
     except ValueError:
-        print('\nError. Probably wrong input.\nInputs for strength and length has to be integer type (number).')
-        password_gen_init()
+        print('\nError. Wrong input.\nInputs for strength and length has to be integer type (number).')
+        return password_gen_init()
+
+
+def check_gen_conditions(length, strength):
+    """ Checks if conditions for password generating are correct."""
+
+    if strength > 4:
+        strength = 4  # Reduce too high strength level to maximum level
+        print('\nNOTE: Given password strength was too high and it was reduced to maximum level, level 4.')
+
+    if strength < 1 or length < 1:
+        if strength < 1:
+            print('\nNOTE: Given strength should be in range 1 - 4.')
+        if length < 1:
+            print('\nNOTE: Password length should be at least 1 (for level 1 strength).')
+        return False
+    elif length < strength:
+        print('\nNOTE: You gave wrong password length according to its strength.'
+              '\n\t  Length should me at least equal to strength.')
+        return False
+    else:
+        return True
 
 
 def generate_password(length, strength):
-    """
+    """Generates password.
+
     Generates password characters base due to chosen strength level
     than generates password with given length and strength level.
 
@@ -36,20 +71,11 @@ def generate_password(length, strength):
     :param strength: integer 1 - 4 inclusive
     :return: string - generated password
     """
+
     digit = string.digits
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
     symbol = string.punctuation
-
-    if strength > 4:
-        print('\nGiven password strength was too high and it was reduced to level 4.')
-
-    if strength < 1 or length < 1:
-        if strength < 1:
-            print('Given strength should be 1 - 4.')
-        if length < 1:
-            print('Password length should be at least 1 (for level 1 strength).')
-        password_gen_init()
 
     while True:
         if strength == 1 and length >= 1:
@@ -61,40 +87,43 @@ def generate_password(length, strength):
         elif strength >= 4 and length >= 4:
             pwd_base = lower + upper + digit + symbol
         else:
-            return print('\nSomething went wrong. Probably you gave wrong password length'
-                         ' according to its strength.')
+            raise ValueError('Something is wrong. Please double-check your arguments for generating password. '
+                             '\nLength should be at least equal to strength.')
 
         password = ''.join(choice(pwd_base) for _ in range(length))
 
         if check_password(password, strength, length):
             return password
+        else:
+            continue
 
 
 def check_password(pwd, strength, length):
-    """
+    """Checks generated password.
+
     Checks if generated password is correct.
     Correct password contains at least one
     character from each of characters group
     for chosen strength level.
 
-    :param pwd: string - generated password
+    :param pwd: string -> generated password
     :param strength: integer 1-4
     :param length: integer >= 1
-    :return: bool - True if password correct, False if not.
+    :return: bool -> True if password correct, False if not.
     """
     if strength == 1 and length >= 1:
         if any(c.islower() for c in pwd):
-            return pwd
+            return True
 
     elif strength == 2 and length >= 2:
         if any(c.islower() for c in pwd) and any(c.isupper() for c in pwd):
-            return pwd
+            return True
 
     elif strength == 3 and length >= 3:
         if (any(c.islower() for c in pwd)
                 and any(c.isupper() for c in pwd)
                 and sum(c.isdigit() for c in pwd) >= 1):
-            return pwd
+            return True
 
     elif strength >= 4 and length >= 4:
         if (any(c.islower() for c in pwd)
@@ -102,6 +131,7 @@ def check_password(pwd, strength, length):
                 and sum(c.isdigit() for c in pwd) >= 1
                 and any(c in string.punctuation for c in pwd)):
             return True
+
     return False
 
 
@@ -114,4 +144,5 @@ if __name__ == '__main__':
           '\n4. lowercase, uppercase, digits and punctuation - 4 characters'
           )
 
-    password_gen_init()
+    outcome_message = password_gen_init()
+    print(outcome_message)
